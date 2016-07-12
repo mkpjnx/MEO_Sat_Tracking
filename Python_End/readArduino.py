1e1a4a484bfa91174f5b2b44c5af9fcc57d71c17
Mike Xu <xpj2000@gmail.com>
2016-07-12 12:09:46 -0500

    Improved NMEA sentence communication

--- Python_End/readArduino.py
+++ Python_End/readArduino.py
@@ -1,5 +1,5 @@
 import serial
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 while True:
-    line = ser.readline().decode("ascii")
-    print(str(line))
+    line = ser.readline().decode("ascii").replace('\r', '').split(',')
+    print(line)
