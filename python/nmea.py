adb6231dc5d7fd15158a7befb4bd0d80fff9b98c
Mike Xu <xpj2000@gmail.com>
2016-07-13 13:58:33 -0500

    Reworked gps.py to fit our solution's needs, added checksum validation

--- python/nmea.py
+++ python/nmea.py
@@ -1,27 +1,11 @@
-"""Access a GPS module through a serial port and parse the data.
+"""Slightly modified version of the original GPS.py code
 
-A python library to accessing a GPS module through a serial port and parsing
-the data for desirable information.
-
-In order for it to work properly, there are a few settings you need to
-manipulate in your GPS and your code
-1. Set GPS unit to output GPRMC (Recommended Minimum) data only
-
-2. Set the output frequency of your GPS to coincide with the sampling interval
-in your code in order to properly read all lines sent by the GPS without buffer
-
-3. Make sure to call the refresh() function of the GPS after every sampling
-iteration in your code in order to properly access the most recent information
-sent by the GPS
-
-4. You should only try to read data from the GPS if the fix state is True, but
-if for any reason the GPS sends strange data the class will return None for any
-missing values
+The nmea class takes in a $GPRMC sentence and parses it for information
 """
 
-import serial
 import time
 import math
+import ephem
 
 def toDeg(x):
     return x * 180 / math.pi
@@ -30,7 +14,7 @@ def toRad(x):
     return x * math.pi / 180
 
 def lenLatLon(lat): #Return length of one degree of latitude and longitude (in meters!!!) as a function of latitude
-    
+
     #Constants for Fourier series approximation
     a1 = 111132.92
     a2 = -559.82
@@ -39,11 +23,11 @@ def lenLatLon(lat): #Return length of one degree of latitude and longitude (in m
     b1 = 111412.84
     b2 = -93.5
     b3 = 0.118
-	
+
     #Fourier seriers that approximates lengths of one degree of latitude and longitude
     lat_len = a1 + (a2 * math.cos(2 * lat)) + (a3 * math.cos(4 * lat)) + (a4 * math.cos(6 * lat))
     lon_len = (b1 * math.cos(lat)) + (b2 * math.cos(3 * lat)) + (b3 * math.cos(5 * lat))
-    
+
     return lat_len, lon_len
 
 def bearingLL(lat1, lon1, lat2, lon2): #Return bearing of vector between two lat/lon points using linear approximation (In degrees E of N)
@@ -54,7 +38,7 @@ def bearingLL(lat1, lon1, lat2, lon2): #Return bearing of vector between two lat
 
     b = 90 - toDeg(math.atan2(y, x)) #Bearing in degrees East of North
     return b
-    
+
 def distanceLL(lat1, lon1, lat2, lon2): #Return distance between two lat/lon points using linear approximation (In meters)
     lat_len, lon_len = lenLatLon((lat1 + lat2) / 2)
 
@@ -64,39 +48,23 @@ def distanceLL(lat1, lon1, lat2, lon2): #Return distance between two lat/lon poi
     return ((x * x) + (y * y)) ** .5 #Distance between points in meters
 
 
-class GPS:
-    """Provides access to a GPS unit through a serial port.
+class nmea:
+    """reads and parses a $GPRMC sentence.
 
     ----> Set GPS to ouput GPRMC lines only <----
-    Call refresh() function after every sampling iteration to update
-    information
+    Create new nmea
     """
 
-    def __init__(self, port, baud):
-        """Initialize with the serial port and the baudrate.
+    def __init__(self, sentence):
+        '''Initialize with the unparsed sentence'''
+        self.unparsed = sentence
+        self.info = sentence.split(',')
 
-        (you will probably want baudrate to be 9600)
-        """
-        self.ser = serial.Serial(port, baud)
-
-        time.sleep(5)
-        self.ser.flushInput()  # Clear out initial junk lines
-
-        self.info = str(self.ser.readline())[2:].split(',')
-
-    def refresh(self):
-        """Read most recent line sent by GPS into a list.
-
-        (call this function after every sampling iteration)
-        """
-        self.info = str(self.ser.readline())[2:].split(',')
 
     def is_fixed(self):
         """Check fix state of GPS and return True/False."""
         try:
-            self.info.index('A')
-            return True
-
+            return True if self.info[2] == 'A' else False
         except ValueError:
             return False
 
@@ -150,12 +118,12 @@ class GPS:
             return None
 
     def get_date(self):
-        """Return date of latest fix as a formatted string (DD/MM/YY)."""
+        """Return date of latest fix as a formatted string (YYYY/MM/DD)."""
         try:
             if len(self.info[9]) == 6:
                 date_str = self.info[9]
                 # Format date to DD/MM/YY
-                return date_str[0:2] + '/' + date_str[2:4] + '/' + date_str[4:]
+                return "20" + date_str[4:] + '/' + date_str[2:4] + '/' + date_str[0:2]
 
             else:
                 return None
@@ -186,7 +154,7 @@ class GPS:
         except IndexError:
             return None
 
-    def get_magnetic_var(self):  # not sure what these means/how accurate
+    def get_magnetic_var(self):  # not sure what these mean/how accurate
         """Return magnetic variation at location.
 
         Add this variation to your magnetic bearing to get true bearing.
@@ -203,3 +171,12 @@ class GPS:
 
         except IndexError:
             return None
+
+    def checksum(self):
+        a = 0
+        tocheck = self.unparsed[1:self.unparsed.index('*')]
+        for s in tocheck:
+            a ^= ord(s)
+        print(self.unparsed.split('*')[1])
+        print(str(hex(a)))
+        return str(hex(a))[2:] == self.unparsed.split('*')[1]
