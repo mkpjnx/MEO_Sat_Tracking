76e773888dfbec3dd6cf15d4a7d8dea046e40b45
minimus <ckuch@imsa.edu>
2017-03-21 22:00:56 -0700

    Semi-final version? UNTESTED LOL

--- Golden TRTL/sentences.py
+++ Golden TRTL/sentences.py
@@ -131,3 +131,56 @@ class NMEA:
             return self.unparsed.count(',') == commacount
         except TypeError:
             return False
+
+class IMU:
+    """Class to hold the information of the 9-DOF sensor."""
+
+    def __init__(self, sentence: str="$IMU,0,0,0,0,0,0,0,0,0"):
+        """Initialize with the unparsed sentence."""
+        self.unparsed = sentence
+        self.info = sentence.split(',')
+
+    def get_heading(self):
+        """Return heading (E of N) in degrees as a float."""
+        try:
+            return float(self.info[1])
+
+        except:
+            return None
+
+    def get_roll(self):
+        """Return roll (counterclockwise) in degrees as a float."""
+        try:
+            return float(self.info[2])
+
+        except:
+            return None
+
+    def get_pitch(self):
+        """Return pitch (positive downwards) in degrees as a float."""
+        try:
+            return float(self.info[3])
+
+        except:
+            return None
+
+    def get_calibration(self):
+        """Return the calibration values of the sensors."""
+        try:
+            return self.info[4:]
+        except:
+            return None
+
+class ADC:
+    """Class for storing $ADC gain sentences."""
+    def __init__(self, sentence: str="$ADC,0.0"):
+        """Initialize with the unparsed sentence."""
+        self.unparsed = sentence
+        self.info = sentence.split(',')
+
+    def get_gain(self) -> float:
+        """Return the gain as a float."""
+        try:
+            return float(self.info[1])
+        except (ValueError, IndexError):
+            return False
--- Golden TRTL/trtlrisen.py
+++ Golden TRTL/trtlrisen.py
@@ -1,8 +1,8 @@
 """Standalone TRTL algorithm module."""
 import time
+from threading import Thread
 import serial
-import nmea
-import orientation as orient
+import sentences
 import geolists
 
 ARDUINO_PORT = 'COM4'
@@ -25,41 +25,56 @@ def read_message(serial_port: serial.Serial) -> str:
         if len(line) > 0 and line[0] == "$":
             return line
 
-def main() -> None:
-    """Main heading determination algorithm."""
-    position = nmea.NMEA("$GPRMC,0,V,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-    orientation = orient.Orientation("$IMU,0,0,0,0,0,0,0,0,0")
+class AlgorithmThread(Thread):
+    """The algorithm wrapped in a thread."""
+    def __init__(self):
+        """Inherit thread properties."""
+        Thread.__init__(self)
+        self.data = {"heading": 0, "lon": 0, "lat": 0, "gain": 0}
+    def run(self) -> None:
+        """Main heading determination algorithm."""
+        position = sentences.NMEA()
+        gain_sentence = sentences.ADC()
 
-    coords_list = geolists.CoordsList(0, 0)
-    bearing_list = geolists.BearingList(0)
+        coords_list = geolists.CoordsList(0, 0)
+        bearing_list = geolists.BearingList(0)
 
-    last_time = time.time()
+        last_time = time.time()
 
-    time_string = time.strftime("%d-%m-%Y_%H-%M-%S", time.localtime())
+        time_string = time.strftime("%d-%m-%Y_%H-%M-%S", time.localtime())
 
-    with open(time_string + ".csv") as log:
-        log.write("Time, Lat, Lon, Heading, Diffbearing\n")
+        with open(time_string + ".csv") as log:
+            log.write("Time, Lat, Lon, Heading, Diffbearing\n")
 
-        while True:
-            sentence = read_message(ARDUINO)
-            if sentence[:2] == "$G":
-                try:
-                    position = nmea.NMEA(sentence)
-                    coords_list.add_coords(position.get_lat(), position.get_lon())
-                    bearing_list.add_bearing(coords_list.get_current_bearing())
-                except IndexError as gps_error:
-                    print("GPS ERROR: {0}".format(gps_error))
-            elif sentence[:2] == "$I":
-                orientation = orient.Orientation(sentence)
+            while True:
+                sentence = read_message(ARDUINO)
+                if sentence[:2] == "$G":
+                    try:
+                        position = sentences.NMEA(sentence)
+                        if position.is_fixed() and position.get_speed() > 1:
+                            coords_list.add_coords(position.get_lat(), position.get_lon())
+                            bearing_list.add_bearing(coords_list.get_current_bearing())
+                        else:
+                            coords_list.lock()
+                            bearing_list.lock()
+                    except (IndexError, geolists.UnchangedPointError) as gps_error:
+                        print("GPS ERROR: {0}".format(gps_error))
+                elif sentence[:2] == "$A":
+                    gain_sentence = sentences.ADC(sentence)
 
-            lat = str(position.get_lat())
-            lon = str(position.get_lon())
-            heading = str(orientation.get_heading())
-            bearing = str(bearing_list.get_bearing())
 
-            if time.time() - last_time >= 1.0:
-                last_time = time.time()
-                log.write(", ".join([str(x) for x in [lat, lon, heading, bearing]]) + "\n")
 
-if __name__ == '__main__':
-    main()
+                lat = str(position.get_lat())
+                lon = str(position.get_lon())
+                # heading = str(orientation.get_heading())
+                gain = gain_sentence.get_gain()
+                bearing = str(bearing_list.get_bearing())
+
+
+                if time.time() - last_time >= 1.0:
+                    last_time = time.time()
+                    log.write(", ".join([str(x) for x in [lat, lon, bearing, gain]]) + "\n")
+                    self.data = {"heading": bearing, "lon": lon, "lat": lat, "gain": gain}
+
+TRTL = AlgorithmThread()
+TRTL.start()
